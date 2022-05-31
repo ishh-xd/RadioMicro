ddc7c0d669c9371649c2fa1c7d985db9c9c67a53
AD!TYA <88816438+AdityaOP2008@users.noreply.github.com>
2022-05-31 14:22:11 +0530

    Create setup.js

---
+++ src/schema/setup.js
@@ -0,0 +1,15 @@
+const { Schema, model} = require('mongoose');
+
+let Setup = new Schema({
+
+    Guild : String,
+
+    Channel: String,
+
+    Message: String,
+
+    voiceChannel: String,
+
+})
+
+module.exports = model('Setup', Setup);
