9eb96c0c8d0f6a716b6d10e157d096308124c4b4
AD!TYA <88816438+AdityaOP2008@users.noreply.github.com>
2022-05-31 15:04:06 +0530

    Create close.js

---
+++ src/events/Node/close.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "error",
+    run: async (client, name, code, reason) => {
+        client.logger.log(`Lavalink ${name}: Closed, Code ${code}, Reason ${reason || 'No reason'}`, "error");
+    }
+};
