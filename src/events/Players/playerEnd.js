c05558234cc8da48a951d06f2c19f649b5de6b99
AD!TYA <88816438+AdityaOP2008@users.noreply.github.com>
2022-05-31 15:18:16 +0530

    Create playerEnd.js

---
+++ src/events/Players/playerEnd.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: "playerEnd",
+	run: async (client, player) => {
+		if (player.data.get("message") && !player.data.get("message").deleted) player.data.get("message").delete().catch(() => null);
+
+	}
+};
