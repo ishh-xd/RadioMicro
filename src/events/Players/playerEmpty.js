cca44a0bb1a2b918ad2a1c0209f70ca1dd3ee3f4
AD!TYA <88816438+AdityaOP2008@users.noreply.github.com>
2022-05-31 15:17:26 +0530

    Create playerEmpty.js

---
+++ src/events/Players/playerEmpty.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: 'playerEmpty',
+  run: async (client, player) => {
+    if (player.data.get('message') && !player.data.get('message').deleted)
+      player.data
+        .get('message')
+        .delete()
+        .catch(() => null);
+    client.channels.cache
+      .get(player.text)
+   /*  ?.send({
+        embeds: [
+          new MessageEmbed()
+            .setColor(client.embedColor)
+            .setDescription('Live Stream Ended. Leaving the voice channel.')
+            .setTimestamp(),
+        ],
+      });*/
+    player.destroy();
+  },
+};
