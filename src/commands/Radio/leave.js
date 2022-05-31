81256593d5658e4f0fb26d67ebc6d4a169690c90
AD!TYA <88816438+AdityaOP2008@users.noreply.github.com>
2022-05-31 14:34:02 +0530

    Create leave.js

---
+++ src/commands/Radio/leave.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "leave",
+    aliases: ["dc"],
+    category: "Music",
+    description: "Leave voice channel",
+    args: false,
+    usage: "",
+    userPrams: [],
+    botPrams: ["EMBED_LINKS"],
+    owner: false,
+    player: false,
+    inVoiceChannel: true,
+    sameVoiceChannel: true,
+    execute: async (message, args, client, prefix) => {
+        const player = client.manager.players.get(message.guild.id);
+
+        const emojiLeave = message.client.emoji.leave;
+
+        await player.destroy(message.guild.id);
+
+        let thing = new MessageEmbed()
+            .setColor(message.client.embedColor)
+            .setDescription(`\`${emojiLeave}\` | **Left the voice channel.**`);
+        return message.reply({ embeds: [thing] });
+    },
+};
